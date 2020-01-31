d921d5ffa3b62b46a15bb7375def64302e640120
Shannon Morrison <shannonm@netflix.com>
2020-01-31 08:25:31 -0800

    adding requirements for tfp

--- setup.py
+++ setup.py
@@ -8,11 +8,15 @@ setup(
     keywords='riskquant',
     url='https://github.com/Netflix-Skunkworks/riskquant/browse',
     setup_requires=['setupmeta'],
-    python_requires='>=3.4',
+    python_requires='>=3.5, <3.8',
     install_requires=[
         'matplotlib',
         'numpy',
-        'scipy'
+        'scipy',
+        # Tensorflow probability is tested and stable against Tensorflow 2.1.0
+        # https://github.com/tensorflow/probability/releases
+        'tensorflow >= 2.1.0',
+        'tensorflow_probability'
     ],
     extras_require={
         'test': ['tox'],
